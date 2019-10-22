5d5918bfafc6876052eaeee1b0be74ebb54caaf2
Ian MacLeod <ian@nevir.net>
2019-10-21 17:35:33 -0700

    gpg agent

--- dot_zshrc.d/plugins/gpg-agent.sh
+++ dot_zshrc.d/plugins/gpg-agent.sh
@@ -1,3 +1,7 @@
+<<chezmoi_bootstrap >/dev/null
+  brew_ensure_latest gnupg
+chezmoi_bootstrap
+
 # https://github.com/sorin-ionescu/prezto/tree/master/modules/gpg
 zplugin ice wait lucid
 zplugin snippet PZT::modules/gpg/init.zsh
