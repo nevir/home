0a99a49a1cf6cf2e842b43c38c508fc5d0905720
Ian MacLeod <ian@nevir.net>
2019-10-22 13:37:24 -0700

    Merge history modules

--- dot_zshrc.d/plugins/history.sh
+++ dot_zshrc.d/plugins/history.sh
@@ -1,8 +1,17 @@
-# History
+# Baseline Configuration
 HISTFILE=~/.zhistory
 HISTSIZE=10000
 SAVEHIST=10000
 setopt hist_expire_dups_first
-setopt hist_ignore_dups
+setopt hist_ignore_all_dups
 setopt hist_ignore_space
 setopt share_history
+
+# https://github.com/zsh-users/zsh-history-substring-search
+zplugin ice wait lucid
+zplugin load zsh-users/zsh-history-substring-search
+
+export HISTORY_SUBSTRING_SEARCH_FUZZY=yes
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
