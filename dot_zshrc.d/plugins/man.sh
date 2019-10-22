b6bc0ebc3f2b9cc2a3a262663b05455ce3cda185
Ian MacLeod <ian@nevir.net>
2019-10-22 12:37:56 -0700

    Colored man pages

---
+++ dot_zshrc.d/plugins/man.sh
@@ -0,0 +1,3 @@
+# https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/colored-man-pages
+zplugin ice wait lucid
+zplugin snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
