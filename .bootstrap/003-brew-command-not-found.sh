4dfa4d3056986de37b2c069b5f55465baa3cd681
Ian MacLeod <ian@nevir.net>
2019-10-21 17:35:11 -0700

    Move command-not-found to a bootstrap in the plugin

--- .bootstrap/003-brew-command-not-found.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-brew tap homebrew/command-not-found
--- dot_zshrc.d/plugins/command-not-found.sh
+++ dot_zshrc.d/plugins/command-not-found.sh
@@ -1,3 +1,7 @@
+<<chezmoi_bootstrap >/dev/null
+  brew tap homebrew/command-not-found
+chezmoi_bootstrap
+
 # https://github.com/sorin-ionescu/prezto/tree/master/modules/command-not-found
 zplugin ice wait lucid
 zplugin snippet PZT::modules/command-not-found/init.zsh
