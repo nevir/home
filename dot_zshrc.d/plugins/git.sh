1b0c0cb6d0280e42b6cdcb7027225d0e1706d0ee
Ian MacLeod <ian@nevir.net>
2019-10-22 13:40:48 -0700

    Add git

---
+++ dot_zshrc.d/plugins/git.sh
@@ -0,0 +1,9 @@
+<<chezmoi_bootstrap >/dev/null
+  brew_ensure_latest hub
+chezmoi_bootstrap
+
+# https://github.com/knu/zsh-git-escape-magic
+zplugin snippet https://raw.githubusercontent.com/knu/zsh-git-escape-magic/master/git-escape-magic
+
+# https://github.com/github/hub
+alias git=hub
