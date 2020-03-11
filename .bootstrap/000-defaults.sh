34981f50562e5e73b48b2e10ba11a47b5377e719
Ian MacLeod <ian@nevir.net>
2020-03-11 12:30:50 -0700

    Add some defaults

---
+++ .bootstrap/000-defaults.sh
@@ -0,0 +1,6 @@
+# System Preferences
+
+# General
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+defaults write NSGlobalDomain AppleAccentcolor -string "-1"
+defaults write NSGlobalDomain AppleHighlightColor -string "0.847059 0.847059 0.862745 Graphite"
