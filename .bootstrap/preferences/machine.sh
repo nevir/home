0f5dbbe25138f2189cf09c9f4199c184a6062518
Ian MacLeod <ian@nevir.net>
2020-03-11 14:24:12 -0700

    Less sudo

--- .bootstrap/preferences/machine.sh
+++ .bootstrap/preferences/machine.sh
@@ -1,8 +1,16 @@
 # Machine Name
-sudo scutil --set ComputerName "$USERNAME"
-sudo scutil --set HostName "$USERNAME"
-sudo scutil --set LocalHostName "$USERNAME"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$USERNAME"
+if [[ "$(scutil --get ComputerName)" != "$USERNAME" ]]; then
+  sudo scutil --set ComputerName "$USERNAME"
+fi
+if [[ "$(scutil --get HostName)" != "$USERNAME" ]]; then
+  sudo scutil --set HostName "$USERNAME"
+fi
+if [[ "$(scutil --get LocalHostName)" != "$USERNAME" ]]; then
+  sudo scutil --set LocalHostName "$USERNAME"
+fi
+if [[ "$(defaults read /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName)" != "$USERNAME" ]]; then
+  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$USERNAME"
+fi
 
 # Trackpad tap to click
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
