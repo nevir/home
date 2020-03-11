665fa66acfbb3bc47e4f85375767ff1f2650781f
Ian MacLeod <ian@nevir.net>
2020-03-11 15:23:24 -0700

    Trackpad sensitivity

--- .bootstrap/preferences/machine.sh
+++ .bootstrap/preferences/machine.sh
@@ -12,10 +12,16 @@ if [[ "$(defaults read /Library/Preferences/SystemConfiguration/com.apple.smb.se
   sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$USERNAME"
 fi
 
-# Trackpad tap to click
+# Trackpad
+
+# Tap to click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
-# And two finger right click
+
+# Two finger right click
 defaults write NSGlobalDomain ContextMenuGesture -int 1
 defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -int 1
+
+# Sensitivity
+defaults write NSGlobalDomain com.apple.trackpad.scaling -string "0.875"
