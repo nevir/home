76c3bbeb65b67a6185c772dc15d0e77be89c9e50
Ian MacLeod <ian@nevir.net>
2020-03-11 12:42:45 -0700

    Trackpad prefs

--- .bootstrap/000-preferences.sh
+++ .bootstrap/000-preferences.sh
@@ -1,3 +1,7 @@
+# See
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# https://github.com/keith/dotfiles/blob/master/osx/defaults.sh
+
 for script in $(ls ./*.sh); do
   source ./"${script}"
 done
--- .bootstrap/preferences/machine.sh
+++ .bootstrap/preferences/machine.sh
@@ -1,4 +1,10 @@
+# Machine Name
 sudo scutil --set ComputerName "$USERNAME"
 sudo scutil --set HostName "$USERNAME"
 sudo scutil --set LocalHostName "$USERNAME"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$USERNAME"
+
+# Trackpad tap to click
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
