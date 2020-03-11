e560cdd8086c24a406b1c0797c4fbfe35a745dcb
Ian MacLeod <ian@nevir.net>
2020-03-11 12:45:53 -0700

    Fix pathing for preferences

--- .bootstrap/000-preferences.sh
+++ .bootstrap/000-preferences.sh
@@ -2,6 +2,6 @@
 # https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 # https://github.com/keith/dotfiles/blob/master/osx/defaults.sh
 
-for script in $(ls ./*.sh); do
-  source ./"${script}"
+for script in $(ls ./.bootstrap/*.sh); do
+  source ./.bootstrap/"${script}"
 done
