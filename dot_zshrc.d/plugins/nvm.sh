24fa687783f758987c9e971fb1d6d97b160701a7
Ian MacLeod <ian@nevir.net>
2020-03-11 17:46:18 -0700

    Configure node

--- dot_zshrc.d/plugins/nvm.sh
+++ dot_zshrc.d/plugins/nvm.sh
@@ -1,6 +1,13 @@
 # https://github.com/lukechilds/zsh-nvm
-zinit ice wait'0.25' lucid
+zinit ice wait'0.25' lucid atload'ensure-node'
 zinit load lukechilds/zsh-nvm
 
 export NVM_AUTO_USE=true
 export NVM_LAZY_LOAD=true
+
+ensure-node() {
+  local version="$(nvm version default 2>&1)"
+  if [[ ! "$version" =~ ^v[0-9.]+$ ]]; then
+    nvm install "$(cat $NVM_DIR/alias/default)"
+  fi
+}
