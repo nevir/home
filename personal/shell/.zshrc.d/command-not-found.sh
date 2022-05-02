7a8519151a4c0158ba312bcf12aefdfa6131e1c0
Ian MacLeod <ian@nevir.net>
2022-05-01 21:33:53 -0700

    More zsh modules

---
+++ personal/shell/.zshrc.d/command-not-found.sh
@@ -0,0 +1,3 @@
+# https://github.com/sorin-ionescu/prezto/tree/master/modules/command-not-found
+zinit ice wait lucid
+zinit snippet PZT::modules/command-not-found/init.zsh
---
+++ personal/shell/.zshrc.d/syntax-highlighting.sh
@@ -0,0 +1,3 @@
+# https://github.com/zsh-users/zsh-syntax-highlighting
+zinit ice wait lucid
+zinit light zsh-users/zsh-syntax-highlighting
