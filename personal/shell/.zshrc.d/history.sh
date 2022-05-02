d62bf0ec6d64a543528ee4f805332ac952cb2d3c
Ian MacLeod <ian@nevir.net>
2022-05-01 21:17:55 -0700

    zsh history config

---
+++ personal/shell/.zshrc.d/history.sh
@@ -0,0 +1,17 @@
+# Baseline Configuration
+HISTFILE=~/.zhistory
+HISTSIZE=10000
+SAVEHIST=10000
+setopt hist_expire_dups_first
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt share_history
+
+# https://github.com/zsh-users/zsh-history-substring-search
+zinit ice wait lucid
+zinit light zsh-users/zsh-history-substring-search
+
+export HISTORY_SUBSTRING_SEARCH_FUZZY=yes
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
